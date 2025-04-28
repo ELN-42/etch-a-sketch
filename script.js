8428c4d53f7473e4c83ed2f644cc48469aec318f
Laurin Erdmann <erdmann2000@gmx.de>
2025-04-28 19:45:50 +0200

    added functions to reset and build the grid
    user can now press the button to reset and build a new grid

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
-let value = 16;
-let container = document.querySelector("#container");
-let percentwidth = (100/value) + "%";
-console.log(percentwidth);
-let max = value*value;
-for(let i = 0; i<max; i++){
-    let item = document.createElement('div');
-    item.style.width = percentwidth;
-    item.style.height = percentwidth;
-    item.style.backgroundColor = "red";
-    item.addEventListener("mouseover" , (event) => event.target.style.backgroundColor = "green");
-    container.append(item);
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+function buildgrid(value){
+    while(value>100){
+        alert("The given size is greater than 100. Please enter a new Value.");
+        value = prompt("Enter the Grid size", 16);
+    }
+    let max = value*value;
+    let percentwidth = (100/value) + "%";
+    let container = document.querySelector("#container");
+    container.style.opacity = 1;
+    while(container.firstChild){
+        container.removeChild(container.lastChild);
+    }
+    for(let i = 0; i<max; i++){
+        let item = document.createElement('div');
+        item.style.width = percentwidth;
+        item.style.height = percentwidth;
+        item.style.backgroundColor = "red";
+        //item.addEventListener("mouseover" , (event) => event.target.style.backgroundColor = "rgb("+getRandomInt(256)+","+getRandomInt(256)+","+getRandomInt(256)+")");
+        item.addEventListener("mouseover" , (event) => stylecolor(container, event.target));
+        container.append(item);
+    }
 }
+
+function stylecolor(container, item){
+    item.style.backgroundColor = "rgb("+getRandomInt(256)+","+getRandomInt(256)+","+getRandomInt(256)+")";
+    container.style.opacity -= 0.01; 
+}
+
+function resetgrid(){
+    value = prompt("Enter the Grid size", 16);
+    buildgrid(value);
+}
+
+console.log("rgb("+getRandomInt(256)+","+getRandomInt(256)+","+getRandomInt(256)+")");
+buildgrid(16);
+let button = document.querySelector(".reset");
+button.addEventListener("click", resetgrid);
